0244ece871db0f604f7456ec43abbd9a9b2db41c
pnuzhnyy <philip.nuzhnyy@gmail.com>
2011-09-10 15:54:14 -0700

    blah

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
 
+
+STATES = ["Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Marianas Islands","Ohio","Oklahoma","Oregon","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Virgin Islands","Washington","West Virginia","Wisconsin","Wyoming"]
 
 def index(request):
-	return HttpResponse("Green tomatoe")
+	return render_to_response("index.html",{"states":STATES})
