a36e69dd5198458eb003c8c6d0471c1bcf0ff1a2
Gilles Debunne <debunne@google.com>
2011-09-10 22:43:19 -0700

    New Health data

--- categories.js
+++ categories.js
@@ -8,6 +8,7 @@ categories=[
 {id:"Business companies", title:"Business companies", fields: ["Totalnumberoffirms","Blackownedfirmspercent","AmericanIndianandAlaskaNativeownedfirmspercent","Asianownedfirmspercent","NativeHawaiianandOtherPacificIslanderownedfirmspercent","Hispanicownedfirmspercent","Womenownedfirmspercent"]},
 {id:"Business types", title:"Business types", fields: ["Manufacturersshipments","Merchantwholesalersales","Retailsales","Retailsalespercapita","Accommodationandfoodservicessales","Buildingpermits","Federalspending"]},
 {id:"Geography", title:"Geography", fields: ["Landarea","Personspersquaremile","FIPSCode"]},
+{id:"Health", title:"Health", fields: ["ObesityRates","LivingWithHIV","LivingWithAIDS","AIDSDiagnoses"]},
 {id:"Energy sources", title:"Energy sources", fields: ["TotalEnergy","Coal","NaturalGas","Petroleum","TotalFossilFuel","NuclearElectricPower","RenewableEnergy"]},
 {id:"Energy exchanges", title:"Energy exchanges", fields: ["NetInterstateFlow","NetElectricityImports"]},
 {id:"Energy usage", title:"Energy usage", fields: ["EndUseResidential","EndUseCommercial","EndUseIndustrial","EndUseTransportation"]}
